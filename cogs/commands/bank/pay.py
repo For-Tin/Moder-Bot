5922a180bb5c43071553a3fa9a3e9172b556f097
For Tin <fortin099517@gmail.com>
2026-03-04 19:38:25 +0200

    Added Bank System

---
+++ cogs/commands/bank/pay.py
@@ -0,0 +1,47 @@
+import disnake
+from disnake.ext import commands
+
+import supabase
+
+from utils import colors
+
+class PayCommand(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(name="pay", description="")
+    async def pay(self, inter: disnake.ApplicationCommandInteraction, payee: disnake.Member, quantity: float):
+
+        db: supabase.Client = self.bot.db
+        sender_stat = db.table("users").select("discord_id, balance").eq("discord_id", inter.user.id).maybe_single().execute()
+        payee_stat = db.table("users").select("discord_id, balance").eq("discord_id", payee.id).maybe_single().execute()
+
+        if sender_stat is None:
+            sender_stat = db.table("users").insert({"discord_id":inter.user.id, "name":inter.user.display_name, "username":inter.user.name}).execute()
+            sender_stat.data = sender_stat.data[0]
+
+        if payee_stat is None:
+            payee_stat = db.table("users").insert({"discord_id":payee.id, "name":payee.display_name, "username":payee.name}).execute()
+            payee_stat.data = payee_stat.data[0]
+
+        sender_balance: float = sender_stat.data["balance"]
+        payee_balance: float = payee_stat.data["balance"]
+
+        embed = disnake.Embed(color=colors.BLUE, title="Pay", description=f"User {inter.user.mention} transferred money to {payee.mention}: {quantity}**₿**")
+        logs_channel = self.bot.get_channel(1476247810985689139)
+
+        if sender_balance >= quantity:
+            sender_balance -= quantity
+            payee_balance += quantity
+
+            db.table("users").update({"balance": sender_balance}).eq("discord_id", inter.user.id).execute()
+            db.table("users").update({"balance": payee_balance}).eq("discord_id", payee.id).execute()
+
+            await inter.response.send_message("Done!", ephemeral=True)
+            await logs_channel.send(embed=embed)
+        else:
+            await inter.response.send_message("Not enough money!", ephemeral=True)
+
+def setup(bot):
+    bot.add_cog(PayCommand(bot))
--- main.py
+++ main.py
@@ -27,9 +27,13 @@ if __name__ == "__main__":
     bot = Client(command_prefix="bob!", intents=disnake.Intents.all())
 
     bot.load_extension("cogs.commands.help")
+
     bot.load_extension("cogs.commands.mod.kick")
     bot.load_extension("cogs.commands.mod.ban")
+
     bot.load_extension("cogs.commands.bank.balance")
+    bot.load_extension("cogs.commands.bank.pay")
+
     bot.load_extension("cogs.logs.message_logs")
     bot.load_extension("cogs.logs.reaction_logs")
 
